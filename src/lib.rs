df0407c9ff2057185271234ded77886ff62f3f4e
Anharu <haruan2394@gmail.com>
2019-08-13 19:36:56 +0900

    Add: Add connect method

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,16 @@ mod models;
 mod routes;
 mod schema;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub fn test_establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_TEST_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
 pub fn rocket() -> rocket::Rocket {
