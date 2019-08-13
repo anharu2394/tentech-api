d6771921aa5d581ab36657b8e13fc76ea40de91e
Anharu <haruan2394@gmail.com>
2019-08-13 17:48:11 +0900

    Add: Add model modules

---
+++ src/models/user.rs
@@ -0,0 +1,15 @@
+use serde::Serialize;
+use std::time::SystemTime;
+
+#[derive(Queryable, Serialize)]
+pub struct User {
+    pub id: i32,
+    pub token: String,
+    pub username: String,
+    pub nickname: String,
+    pub email: String,
+    pub password: String,
+    pub activated: bool,
+    pub activated_at: Option<SystemTime>,
+    pub expired_at: SystemTime,
+}
