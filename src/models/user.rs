e3dd011315cdd62a5a0cbd24efbedeb59898f1fa
Anharu <haruan2394@gmail.com>
2019-08-14 19:15:18 +0900

    Change: Change sctructure of TokenData

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,9 +1,9 @@
 use crate::email::{send_activation_email, SendError};
 use crate::token::encrypt;
-use chrono::offset::Utc;
+use chrono::offset::Local;
 use chrono::DateTime;
+use chrono::Duration;
 use serde::Serialize;
-use std::time::Duration;
 use std::time::SystemTime;
 
 #[derive(Clone, Queryable, Serialize)]
@@ -19,14 +19,9 @@ pub struct User {
 
 #[derive(Clone, Queryable, Serialize)]
 pub struct TokenData {
-    pub id: i32,
-    pub username: String,
-    pub nickname: String,
-    pub email: String,
-    pub password: String,
-    pub activated: bool,
-    pub activated_at: Option<SystemTime>,
-    pub expired_at: SystemTime,
+    #[serde(flatten)]
+    pub user: User,
+    pub expired_at: DateTime<Local>,
 }
 
 impl User {
@@ -43,16 +38,10 @@ impl User {
         encrypt(&json)
     }
     pub fn to_token_data(&self) -> TokenData {
-        let expired_at = SystemTime::now() + Duration::from_secs(86400);
+        let expired_at = Local::now() + Duration::days(1);
         TokenData {
+            user: self.clone(),
             expired_at,
-            id: self.id,
-            username: self.username.to_string(),
-            nickname: self.nickname.to_string(),
-            email: self.email.to_string(),
-            password: self.password.to_string(),
-            activated: self.activated,
-            activated_at: None,
         }
     }
 }
