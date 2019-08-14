d8726465c4209bf0bb4f3267bc95ce208ad545e5
Anharu <haruan2394@gmail.com>
2019-08-14 13:40:12 +0900

    Add: Add token generation by fernet

---
+++ src/token.rs
@@ -0,0 +1,12 @@
+use fernet::DecryptionError;
+use fernet::Fernet;
+use std::env;
+
+let fernet = fernet::Fernet::new(&env::var("FERNET_SECRET_KEY").unwrap()).unwrap();
+
+pub fn encrypt(secret_phrase: &String) -> String {
+    fernet.encrypt(secret_phrase.as_bytes())
+}
+pub fn decrypt(token: &String) -> Result<Vec<u8>, DecryptionError> {
+    fernet.decrypt(token)
+}
