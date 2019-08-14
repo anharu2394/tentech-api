9ab8b6a9f5d42a69f83b49a381a5315ab977028d
Anharu <haruan2394@gmail.com>
2019-08-14 13:10:23 +0900

    Remove: no use token and expired_at

--- migrations/2019-08-13-051919_create_users/up.sql
+++ migrations/2019-08-13-051919_create_users/up.sql
@@ -1,12 +1,10 @@
 -- Your SQL goes here
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  token VARCHAR UNIQUE NOT NULL,
   username VARCHAR NOT NULL UNIQUE,
   nickname VARCHAR NOT NULL,
   email VARCHAR NOT NULL,
   password VARCHAR NOT NULL UNIQUE,
   activated BOOLEAN NOT NULL,
-  activated_at TIMESTAMP,
-  expired_at TIMESTAMP NOT NULL
+  activated_at TIMESTAMP
 )
--- src/schema.rs
+++ src/schema.rs
@@ -1,13 +1,11 @@
 table! {
     users (id) {
         id -> Int4,
-        token -> Varchar,
         username -> Varchar,
         nickname -> Varchar,
         email -> Varchar,
         password -> Varchar,
         activated -> Bool,
         activated_at -> Nullable<Timestamp>,
-        expired_at -> Timestamp,
     }
 }
