579d7ce337bb7385e9db83b7cdd884f153da6406
Anharu <haruan2394@gmail.com>
2019-08-19 00:28:26 +0900

    Remove: Never used import

--- src/email.rs
+++ src/email.rs
@@ -1,11 +1,8 @@
 use lettre::smtp::authentication::{Credentials, Mechanism};
 use lettre::smtp::extension::ClientId;
 use lettre::smtp::ConnectionReuseParameters;
-use lettre::{EmailAddress, Envelope, SendableEmail, SmtpClient, Transport};
-use lettre_email::EmailBuilder;
-use lettre_email::{mime::TEXT_PLAIN, Email};
-use std::env;
-use std::path::Path;
+use lettre::{SmtpClient, Transport};
+use lettre_email::Email;
 
 pub struct SendError;
 
