9f66c6157ddebd17a7e28d042f86426d52e1bc09
Anharu <haruan2394@gmail.com>
2019-08-18 23:49:24 +0900

    Remove: Remove implemention of Responder for Errors

--- src/errors.rs
+++ src/errors.rs
@@ -24,26 +24,6 @@ impl Errors {
     }
 }
 
-impl<'r> Responder<'r> for Errors {
-    fn respond_to(self, req: &Request) -> response::Result<'r> {
-        let errors = self
-            .errors
-            .field_errors()
-            .into_iter()
-            .map(|(field, errors)| {
-                let codes = errors.into_iter().map(|err| err.code).collect();
-                (field, codes)
-            })
-            .collect::<HashMap<_, Vec<_>>>();
-
-        status::Custom(
-            Status::UnprocessableEntity,
-            Json(json!({ "errors": errors })),
-        )
-        .respond_to(req)
-    }
-}
-
 pub struct FieldValidator {
     errors: ValidationErrors,
 }
