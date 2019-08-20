17bcfad2a0d51e51808b2d3207c3b34e9f6b6c6a
Anharu <haruan2394@gmail.com>
2019-08-20 13:34:00 +0900

    Add: Declare User model

---
+++ src/models/product.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Queryable, Serialize, Deserialize, Identifiable)]
+#[table_name = "products"]
+pub struct User {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub img: String,
+    pub kind: String,
+    pub duration: i32,
+}
