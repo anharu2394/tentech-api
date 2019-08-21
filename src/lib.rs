71f2d810c9239e6307b39d729d7fad0c2fb079b8
Anharu <haruan2394@gmail.com>
2019-08-21 20:53:39 +0900

    Add: Add establish connection

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,12 @@ pub fn test_establish_connection() -> PgConnection {
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
 pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount(
