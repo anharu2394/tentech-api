96b35f8ae56a9348c7ee8cfb4f5e7ab00050bd95
Anharu <haruan2394@gmail.com>
2019-08-25 22:01:47 +0900

    Add: Add processing to upload s3obj

--- src/routes/s3.rs
+++ src/routes/s3.rs
@@ -18,5 +18,26 @@ pub struct NewAssetData {
     attachment: String,
 }
 
+#[derive(Serialize)]
+pub struct CreatedAsset {
+    url: String,
+}
 #[post("/upload", format = "json", data = "<new_asset>")]
-pub fn upload(new_asset: Json<NewAsset>) -> Result<NewAsset, TentechError> {}
+pub fn upload(
+    new_asset: Json<NewAsset>,
+    client: State<S3Client>,
+) -> Result<JsonValue, TentechError> {
+    let new_asset = new_asset.into_inner().asset;
+    let mut request = PutObjectRequest::default();
+    request.bucket = String::from("tentech");
+    request.key = new_asset.key.to_string();
+    let body =
+        base64::decode(&new_asset.attachment).map_err(|_| TentechError::CannotDecodeBase64)?;
+    println!("{}", body.len())
+    request.body = Some(ByteStream::from(body));
+    client
+        .put_object(request)
+        .sync()
+        .map(|a| json!({ "asset": CreatedAsset { url: format!("https://tentech.s3-ap-northeast-1.amazonaws.com/{}",new_asset.key)}}))
+        .map_err(|_| TentechError::CannotPutS3Object)
+}
