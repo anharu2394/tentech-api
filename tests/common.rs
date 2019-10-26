ad5286fb91f6650c9c18c64ed33ea24054d9e484
Anharu <haruan2394@gmail.com>
2019-10-26 18:07:40 +0900

    Add: Add useful fn for testingx

--- tests/common.rs
+++ tests/common.rs
@@ -1,8 +1,48 @@
-use crate::rocket;
-use rocket::local::Client;
+use tentech_api::rocket;
+use tentech_api::db;
+use tentech_api::test_establish_connection;
+use tentech_api::schema::users;
+use ::rocket::local::Client;
+use ::rocket::Response;
 use std::env;
+use serde_json::Value;
+use diesel::dsl::*;
+use diesel::prelude::*;
+
+pub const PASSWORD: &'static str = "passpassword";
+
+#[macro_export]
+macro_rules! json_string {
+    ($value:tt) => {
+        serde_json::to_string(&serde_json::json!($value)).expect("cannot json stringify")
+    };
+}
 
 pub fn test_client() -> Client {
     env::set_var("ROCKET_ENV", "stage");
     Client::new(rocket()).expect("valid rocket instance")
+}
+
+pub fn delete_all_users() {
+    db::users::delete_all(&test_establish_connection()).expect("failed");
+    let count = users::table.select(count_star()).first(&test_establish_connection());
+    assert_eq!(Ok(0), count);
+}
+
+pub fn see_user(name: &String) {
+    match db::users::find_by_username(&test_establish_connection(), name) {
+        Ok(r) => println!("{}", json_string!(r)),
+        Err(e) => println!("{:?}", e)
+    }
+}
+
+pub fn create_user(name: &str) {
+    db::users::create(&test_establish_connection(), &(name.to_owned() + "_test"), &(name.to_owned() + "_test"),&(name.to_owned() + "_test@test.com"),PASSWORD).expect("failed");
+}
+
+pub fn user_json(name: &str) -> Value{
+    serde_json::json!({"username": &(name.to_owned() + "_test"), "nickname": &(name.to_owned() + "_test"), "email":&(name.to_owned() + "_test@test.com"), "password": PASSWORD})
+}
+pub fn print_response(response: &mut Response) {
+    println!("{}", response.body_string().unwrap())
 }
